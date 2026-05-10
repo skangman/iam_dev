84354434acaaa545efee65374fe0bad770735d59
skangman <orasa_k@uih.co.th>
2026-05-10 10:01:02 +0700

    Fix Vercel CI build warnings

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -6,8 +6,6 @@ class Header extends Component {
   render() {
     if (!this.props.data) return null;
 
-    const project = this.props.data.project;
-    const github = this.props.data.github;
     const name = this.props.data.name;
     const description = this.props.data.description;
     const theme = this.props.theme || "light";
